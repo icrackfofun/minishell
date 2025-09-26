132936a6c56247a74b718c4a1831ea516ab2a8cb
icrackfofun <miguelred2009@gmail.com>
2025-09-26 18:59:43 +0100

    .

--- signals.c
+++ signals.c
@@ -6,7 +6,7 @@
 /*   By: psantos- <psantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 15:23:38 by psantos-          #+#    #+#             */
-/*   Updated: 2025/09/26 18:37:10 by psantos-         ###   ########.fr       */
+/*   Updated: 2025/09/26 18:53:38 by psantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ void	kill_all_children(t_info *info)
 void	prompt_sigint_handler(int sig)
 {
 	(void)sig;
-	g_last_signal = SIGINT;
 	write(STDOUT_FILENO, "\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
@@ -44,14 +43,11 @@ void	prompt_sigint_handler(int sig)
 void	child_sigint_handler(int sig)
 {
 	(void)sig;
-	sleep(100);
 	child_exit("", 2, &terminal()->info, "");
 }
 
 void	child_sigquit_handler(int sig)
 {
 	(void)sig;
-	sleep(100);
 	child_exit("", 3, &terminal()->info, "");
 }
-
