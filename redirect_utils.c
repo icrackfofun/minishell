c621f249bd41d96727b2aa47b93d21ca41868989
icrackfofun <miguelred2009@gmail.com>
2025-09-27 12:01:39 +0100

    .

--- redirect_utils.c
+++ redirect_utils.c
@@ -6,7 +6,7 @@
 /*   By: psantos- <psantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 14:36:27 by psantos-          #+#    #+#             */
-/*   Updated: 2025/09/27 11:51:34 by psantos-         ###   ########.fr       */
+/*   Updated: 2025/09/27 12:01:32 by psantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,7 @@ static void	write_heredoc_to_tmp(const char *delimiter, char *filename,
 			child_exit("malloc", 1, info, "");
 		write(fd, expanded, ft_strlen(info->line));
 		write(fd, "\n", 1);
+		free(expanded);
 	}
 	close (fd);
 }
