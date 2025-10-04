d8f446a782aafce956b5396de8b44c54751569e5
icrackfofun <miguelred2009@gmail.com>
2025-10-04 22:45:25 +0100

    .

--- srcs/executor/command.c
+++ srcs/executor/command.c
@@ -6,7 +6,7 @@
 /*   By: psantos- <psantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 16:07:55 by psantos-          #+#    #+#             */
-/*   Updated: 2025/10/04 22:41:50 by psantos-         ###   ########.fr       */
+/*   Updated: 2025/10/04 22:44:49 by psantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ static void exec_sh(char *path, t_ast *cmd, t_info *info)
 	len = ft_strlen(path);
 	if (len > 3 && !ft_strcmp(path + len - 3, ".sh"))
 	{
-		if (stat(path, &st) == -1)
+		if (stat(path, &st) == -1 || access(path, R_OK) == -1)
 			return ;
 		args[0] = "sh";
 		args[1] = path;
